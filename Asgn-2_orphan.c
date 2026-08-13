c177a4c3b3d5f93d3dd2b5f9cb36f77d9a2ad4ad
Aryan Jha <aryanjha3137@gmail.com>
2026-08-13 14:35:38 +0530

    Refactor orphan process example in C

--- Asgn-2_orphan.c
+++ Asgn-2_orphan.c
@@ -1,62 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
 
-int main() {
-    int pipefd[2];
-    pid_t pid;
-    char message[100];
-    char buffer[100];
-
-
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
-        return 1;
-    }
-
-
-    pid = fork();
-
-    if (pid < 0) {
-        perror("fork");
-        return 1;
-    }
-
-
-    if (pid > 0) {
-
-        close(pipefd[0]);  
-
-
-        printf("Enter a message: ");
-        fgets(message, sizeof(message), stdin);
-
-
-        message[strcspn(message, "\n")] = '\0';
-
-
-        write(pipefd[1], message, strlen(message) + 1);
-
-        close(pipefd[1]); 
-
-
-        wait(NULL);
-    }
-
-
-    else {
-
-        close(pipefd[1]);  
-
-        read(pipefd[0], buffer, sizeof(buffer));
-
-        printf("Child received: %s\n", buffer);
-
-        close(pipefd[0]);  
-    }
-
-    return 0;
-}
+int main() 
+	{
+		int pid = fork();
+		
+		if(pid < 0)
+                printf("Fork failed\n");
+		
+		if (pid>0){
+			printf("parent process id %d\n",getpid());
+			printf("child process id %d\n",pid);
+		}
+		else if (pid == 0){
+			sleep(10);
+			printf("child process id %d\n",getpid());
+			printf("parent process id %d\n",getppid());
+		}
+	return 0;
+	}
