a87f9562ae4f676bef61bba248ecb18c2914be87
Aryan Jha <aryanjha3137@gmail.com>
2026-08-13 14:26:28 +0530

    Update and rename orphan.c to Asgn-2_orphan.c

---
+++ Asgn-2_orphan.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    int pipefd[2];
+    pid_t pid;
+    char message[100];
+    char buffer[100];
+
+
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        return 1;
+    }
+
+
+    pid = fork();
+
+    if (pid < 0) {
+        perror("fork");
+        return 1;
+    }
+
+
+    if (pid > 0) {
+
+        close(pipefd[0]);  
+
+
+        printf("Enter a message: ");
+        fgets(message, sizeof(message), stdin);
+
+
+        message[strcspn(message, "\n")] = '\0';
+
+
+        write(pipefd[1], message, strlen(message) + 1);
+
+        close(pipefd[1]); 
+
+
+        wait(NULL);
+    }
+
+
+    else {
+
+        close(pipefd[1]);  
+
+        read(pipefd[0], buffer, sizeof(buffer));
+
+        printf("Child received: %s\n", buffer);
+
+        close(pipefd[0]);  
+    }
+
+    return 0;
+}
